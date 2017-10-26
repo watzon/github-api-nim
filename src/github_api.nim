ddc61acbfa445c2133a894ea2cf410b02ed3c97a
Chris Watson <chris@watzon.me>
2017-10-26 07:21:39 -0700

    Remove test code

--- src/github_api.nim
+++ src/github_api.nim
@@ -3,9 +3,9 @@ import ./github_api/client
 import ./github_api/repository
 import ./github_api/gist
 
-var cli = newGithubApiClient(accessToken = "5634974f1b611f092ba91ec7ce8eb593f11aec67")
-var response = cli.deleteGist("d8b6d133fa33eef1aeacd5adee9cf864")
+# var cli = newGithubApiClient(accessToken = "")
+# var response = cli.deleteGist("d8b6d133fa33eef1aeacd5adee9cf864")
 
-echo(parseJson(readAll(response.bodyStream)).pretty())
+# echo(parseJson(readAll(response.bodyStream)).pretty())
 
 
